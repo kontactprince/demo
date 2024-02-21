5e18d1ef145b5f08f0316638b4fca4d791a3c11d
kontactprince <94216295+kontactprince@users.noreply.github.com>
2024-02-21 09:18:45 +0000

    update sring

---
+++ pythonclass/day22.py
@@ -0,0 +1,11 @@
+p="prince"
+print(p[::-1])
+
+#update entire string
+string1="hello,i am prince"
+print("initial string")
+print(string1)
+
+string1="welcome, prince"
+print("\nUpdated string")
+print(string1)
