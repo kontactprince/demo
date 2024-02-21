d4d9afe01f328f2e37758ef1049cceffd96b6ab4
kontactprince <94216295+kontactprince@users.noreply.github.com>
2024-02-21 10:20:37 +0000

    listmethod

---
+++ delete.py
@@ -0,0 +1,7 @@
+string1="hello world"
+print("initial string")
+print(string1)
+
+string2=string1[0:2]+string1[3:]
+print("\nDeleting character at 2nd index: ")
+print(string2)
---
+++ pythonclass/list.py
@@ -0,0 +1,31 @@
+list=[10,20,4,30,54,20,49]
+print("\nList of number: ")
+print(list)
+
+list1=["prince","kumar","yadav"]
+print("\nList items: ")
+print(list1[0])
+print(list1[1])
+list.pop()
+print(list)
+
+list.reverse()
+print(list)
+
+list.insert(0,23)
+print(list)
+
+list.sort()
+print(list)
+
+y=list.count(20)
+print(y)
+
+y2=sum(list)
+print(y2)
+y1=sum(list,45)
+print(y1)
+
+
+
+
