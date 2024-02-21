5b9fa9ed42bb369c6ba5a48fc25b76709e736d68
kontactprince <94216295+kontactprince@users.noreply.github.com>
2024-02-21 08:54:56 +0000

    string in function

---
+++ pythonclass/prince.py
@@ -0,0 +1,12 @@
+print("string")
+string1='i am prince'
+print(string1)
+
+string2='prince'
+print('Inition string: ')
+print(string2)
+print("\nfirst character of string is: ")
+print(string2[0])
+print("\nlast character of string is: ")
+print(string2[-1])
+
